6610bd4758988104b8b4ea715ba8a13bb49e75f2
Shruti Kulkarni <skulkarni3@dons.usfca.edu>
2025-05-02 21:33:06 -0700

    lab 9

--- app/redditApp.py
+++ app/redditApp.py
@@ -49,10 +49,3 @@ def predict(data : request_body):
     predictions = model_pipeline.predict_proba(X)
     return {'Predictions': predictions}
 
-# import requests
-
-# comment = {'reddit_comment':'Testing a comment.'}
-
-# url = 'http://127.0.0.1:8000/predict'
-# response = requests.post(url, json=comment)
-# print(response.json())
